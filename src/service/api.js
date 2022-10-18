99eacc84362b0ab7f582df78888094fd84c86fdd
Thaís Calixto <thais.calixto@mail.uft.edu.br>
2022-10-18 00:47:55 -0300

    Use axios

--- src/service/api.js
+++ src/service/api.js
@@ -1,1 +1,6 @@
+import axios from "axios";
+
 export const avatarAPI = "https://avatars.dicebear.com/api/";
+export const animeAPI = axios.create({
+	baseURL: "https://animechan.vercel.app/api/",
+});
