34745a71b90aafa4f8f9c6c5889f20d3a12658d6
Thaís Calixto <thais.calixto@mail.uft.edu.br>
2022-10-18 01:25:22 -0300

    Stylize error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,16 +1,20 @@
-import { useRouteError } from "react-router-dom";
+import { Alert, Text, Container } from "@mantine/core";
+import { IconAlertCircle } from "@tabler/icons";
+import { useRouteError, Link } from "react-router-dom";
 
 const ErrorPage = () => {
 	const error = useRouteError();
 	console.error(error);
 
 	return (
-		<div className="error-page">
-			<h1>Oops!</h1>
-			<p>
-				<i>{error.statusText || error.message}</i>
-			</p>
-		</div>
+		<Container>
+			<Alert icon={<IconAlertCircle size={16} />} title="Ooops!" color="red">
+				<Text italic>Página não encontrada.</Text>
+				<Text variant="link" component={Link} to="/">
+					Voltar para página incial.
+				</Text>
+			</Alert>
+		</Container>
 	);
 };
 
