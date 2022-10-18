96276f51247075def44a828d38b63e464112058f
Thaís Calixto <thais.calixto@mail.uft.edu.br>
2022-10-18 01:45:50 -0300

    Show quote with avatar on root page

--- src/pages/RootPage.jsx
+++ src/pages/RootPage.jsx
@@ -1,10 +1,25 @@
-import { Container, Title, Group, Button } from "@mantine/core";
+import { Container, Title, Group, Button, Center, Avatar } from "@mantine/core";
 import { Link } from "react-router-dom";
+import { avatarAPI } from "../service/api";
+
+import sprites from "../constants/sprites";
+import BlockQuote from "../components/BlockQuote";
 
 function RootPage() {
 	return (
 		<Container>
 			<Title color="pink">Página Inicial</Title>
+
+			{[...Array(5)].map((_, key) => (
+				<Center key={key}>
+					<Avatar
+						radius="xl"
+						size="lg"
+						src={`${avatarAPI}${sprites[Math.floor(Math.random() * 10)]}/.svg`}
+					/>
+					<BlockQuote />
+				</Center>
+			))}
 
 			<Group position="center" spacing="xl" grow>
 				<Button
