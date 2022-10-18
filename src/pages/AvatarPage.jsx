64b9e7b120b808a7abb911072f6989fb4002e005
Thaís Calixto <thais.calixto@mail.uft.edu.br>
2022-10-18 00:30:49 -0300

    Hooks on avatar page

--- src/pages/AvatarPage.jsx
+++ src/pages/AvatarPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { avatarAPI } from "../service/api";
 
 const AvatarPage = () => {
@@ -14,24 +15,43 @@ const AvatarPage = () => {
 		"micah",
 	];
 
+	const [sprite, setSprite] = useState(sprites[0]);
+	const [seed, setSeed] = useState();
+	const [avatar, setAvatar] = useState();
+
 	return (
 		<>
 			<h1>Criar Avatar</h1>
 			<div className="sprites">
 				Escolha um sprite:
-				<select>
+				<select
+					value={sprite}
+					onInput={(e) => setSprite(e.currentTarget.value)}
+				>
 					{sprites.map((element) => (
-						<option>{element}</option>
+						<option key={element}>{element}</option>
 					))}
 				</select>
 			</div>
 
-			<div className="seed">
+			<div
+				className="seed"
+				value={seed}
+				onInput={(e) => setSeed(e.currentTarget.value)}
+			>
 				Entre com uma seed:
 				<input type="text" />
 			</div>
 
-            <button>Enviar</button>
+			<button onClick={() => setAvatar(`${avatarAPI}${sprite}/${seed}.svg`)}>
+				Enviar
+			</button>
+
+			<div className="avatar-image">
+				<figure>
+					<img src={avatar} alt="Avatar gerado" />
+				</figure>
+			</div>
 		</>
 	);
 };
