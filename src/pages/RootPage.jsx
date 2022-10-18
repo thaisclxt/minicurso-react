c04832a2adaa3286d02d3d4f05d1dda0ae6a9ce0
Thaís Calixto <thais.calixto@mail.uft.edu.br>
2022-10-18 01:29:57 -0300

    Stylize root page

--- src/pages/RootPage.jsx
+++ src/pages/RootPage.jsx
@@ -1,11 +1,30 @@
+import { Container, Title, Group, Button } from "@mantine/core";
 import { Link } from "react-router-dom";
 
 function RootPage() {
 	return (
-		<div className="App">
-			<p>Página Inicial</p>
-			<Link to="/avatar">Criar avatar</Link>
-		</div>
+		<Container>
+			<Title color="pink">Página Inicial</Title>
+
+			<Group position="center" spacing="xl" grow>
+				<Button
+					variant="gradient"
+					gradient={{ from: "orange", to: "pink" }}
+					component={Link}
+					to="/avatar"
+				>
+					Criar avatar
+				</Button>
+				<Button
+					variant="gradient"
+					gradient={{ from: "orange", to: "pink" }}
+					component={Link}
+					to="/quote"
+				>
+					Gerar citação
+				</Button>
+			</Group>
+		</Container>
 	);
 }
 
